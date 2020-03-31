8909f0d5881b5ce526970fc505c1e7fb4a193d98
HiraiY <hirai@web-tips.co.jp>
2020-03-31 11:11:44 +0900

    [modify] modified I18n

--- app/controllers/chat/line_report_controller.rb
+++ app/controllers/chat/line_report_controller.rb
@@ -11,7 +11,7 @@ class Chat::LineReportController < ApplicationController
   private
 
   def set_crumbs
-    @crumbs << [I18n.t('chat.line_report'), action: :index]
+    @crumbs << [I18n.t('chat.line_bot.line_report'), action: :index]
   end
 
   def cond
