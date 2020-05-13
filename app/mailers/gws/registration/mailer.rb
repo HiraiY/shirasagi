0e081f1fb446c7c3d26ddd14e6a179ca5d70c07e
HiraiY <hirai@web-tips.co.jp>
2020-05-13 18:00:13 +0900

    [fix] fixed registration mailer page and view page

--- app/mailers/gws/registration/mailer.rb
+++ app/mailers/gws/registration/mailer.rb
@@ -4,24 +4,20 @@ class Gws::Registration::Mailer < ActionMailer::Base
     Rails.application.routes.url_helpers
   end
 
-  # 仮登録の通知メールを送る
-  def notify_mail(user, site, protocol, host)
-    require 'pry'
-    binding.pry
+  # 仮登録ページへの案内メールを送る
+  def verification_mail(user, protocol, host)
     @user = user
-
-    @page_url = url_helpers.gws_user_url(protocol: protocol, host: host, site: site.id, id: user.id)
+    @page_url = url_helpers.gws_registration_index_url(protocol: protocol, host: host, site: user.site_id)
     sender = "a"
 
     mail from: sender, to: user.email
   end
 
-  # 仮登録ページへの案内メールを送る
-  def verification_mail(user, protocol, host)
-    require 'pry'
-    binding.pry
+  # 仮登録の通知メールを送る
+  def notify_mail(user, site, protocol, host)
     @user = user
-    @page_url = url_helpers.gws_registration_index_url(protocol: protocol, host: host, site: user.site_id)
+    @site = site
+    @page_url = url_helpers.gws_user_url(protocol: protocol, host: host, site: site.id, id: user.id)
     sender = "a"
 
     mail from: sender, to: user.email
@@ -31,8 +27,6 @@ class Gws::Registration::Mailer < ActionMailer::Base
   def approval_mail(user, protocol, host)
     @user = user
     @page_url = url_helpers.sns_login_url(protocol: protocol, host: host)
-    require 'pry'
-    binding.pry
     sender = "a"
 
     mail from: sender, to: user.email
