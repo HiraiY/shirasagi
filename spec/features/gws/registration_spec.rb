116b9fb91a4f0228c0c369ec40cb0c94a87147f6
HiraiY <hirai@web-tips.co.jp>
2020-05-19 13:39:01 +0900

    [wip] rspec test

--- spec/features/gws/registration_spec.rb
+++ spec/features/gws/registration_spec.rb
@@ -6,6 +6,8 @@ describe "gws_registration", type: :feature, dbscope: :example do
   let(:group) { gws_user.groups.first }
   let(:role) { create(:gws_role_admin) }
   let(:email) { "#{unique_id}@example.jp" }
+  let(:name) { unique_id }
+  let(:password) { "abc123" }
   let(:sender_email) { "noreply@example.jp" }
   let(:gws_site_path) { gws_site_path(site: site) }
   let(:gws_edit_path) { edit_gws_site_path(site: site) }
@@ -13,14 +15,6 @@ describe "gws_registration", type: :feature, dbscope: :example do
   let(:login_path) { gws_login_path(site: site) }
   let(:logout_path) { gws_logout_path(site: site) }
   let(:main_path) { gws_portal_path(site: site) }
-
-  before do
-    ActionMailer::Base.deliveries = []
-  end
-
-  after do
-    ActionMailer::Base.deliveries = []
-  end
 
   context "new" do
     it do
@@ -74,8 +68,28 @@ describe "gws_registration", type: :feature, dbscope: :example do
       mail = ActionMailer::Base.deliveries.first
       expect(mail.from.first).to eq sender_email
       expect(mail.to.first).to eq email
-      expect(mail.subject).to eq '[仮登録のご案内]'
-      expect(mail.body.raw_source).to match(/http(s|):\/\/[\w\-\_\.\!\*\'\)\(]+/)
+      expect(mail.subject).to eq "[仮登録のご案内]"
+      expect(mail.body.raw_source).to have_content URI.extract(mail.body.raw_source, ["http"]).first
+      url = URI.extract(mail.body.raw_source, ["http"]).first
+      visit url
+      within "form" do
+        fill_in "item[name]", with: name
+        fill_in "item[in_password]", with: password
+        fill_in "item[in_password_again]", with: password
+        click_button "登録"
+      end
+      expect(page).to have_content "仮登録の申請をしました。"
+      expect(ActionMailer::Base.deliveries.length).to eq 2
+      notify_mail = ActionMailer::Base.deliveries.last
+      expect(notify_mail.from.first).to eq email
+      expect(notify_mail.to.first).to eq sender_email
+      expect(notify_mail.subject).to eq "[仮登録申請]#{name} - #{site.name}"
+      expect(notify_mail.body.raw_source).to have_content URI.extract(notify_mail.body.raw_source, ["http"]).first
+      url = URI.extract(notify_mail.body.raw_source, ["http"]).first
+      visit url
+      click_on "編集する"
+
+      select "承認", from: "item[temporary]"
       puts page.html
     end
   end
