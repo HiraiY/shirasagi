9de566455c6a373cad50d42a04110ade217f0b06
HiraiY <hirai@web-tips.co.jp>
2020-05-26 20:24:34 +0900

    [fix] fixed to be able to set_center_position at marker position

--- app/models/facility/map.rb
+++ app/models/facility/map.rb
@@ -9,10 +9,20 @@ class Facility::Map
   include Cms::Addon::GroupPermission
 
   default_scope ->{ where(route: "facility/map") }
+  validate :center_position_validate, if: -> { set_center_position.present? }
 
   private
 
   def serve_static_file?
     false
+  end
+
+  def center_position_validate
+    latlon = set_center_position.split(',').map(&:to_f)
+    lat = latlon[0]
+    lon = latlon[1]
+    unless lat >= -90 && lat <= 90 && lon >= -180 && lon <= 180
+      self.errors.add :set_center_position, :invalid_latlon
+    end
   end
 end
